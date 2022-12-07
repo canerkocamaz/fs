ce43686e2359e1d7bf31b8ca12c1efc3ec7d7bfb
canerkocamaz <kocamaz.caner@gmail.com>
2022-12-07 21:29:02 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace FolderSplitter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.hakkındaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(86, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "böl parçala fethet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(83, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bölünme adedi giriniz....";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hakkındaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(374, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // hakkındaToolStripMenuItem
+            // 
+            this.hakkındaToolStripMenuItem.Name = "hakkındaToolStripMenuItem";
+            this.hakkındaToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.hakkındaToolStripMenuItem.Text = "Hakkında";
+            this.hakkındaToolStripMenuItem.Click += new System.EventHandler(this.hakkındaToolStripMenuItem_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 179);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(374, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 202);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Klasör Bölücü";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hakkındaToolStripMenuItem;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace FolderSplitter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int sayac=0;
+            int bolunme_adedi=999999999;
+            //int dosya_sayisi = 10;
+
+            if (String.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("bölünme adedi giriniz...");
+                
+            }
+            else
+            {
+                bolunme_adedi = Int32.Parse(textBox1.Text);
+                progressBar1.Minimum = 1;
+                
+                DialogResult dr = folderBrowserDialog1.ShowDialog();
+                string foldername = "";
+                string dizin;
+
+                if (dr == DialogResult.OK)
+                {
+                    foldername = folderBrowserDialog1.SelectedPath;
+
+
+                }
+                else
+                {
+                    return;
+                }
+                DirectoryInfo d = new DirectoryInfo(foldername);
+                dizin = d.Name;
+                FileInfo[] file = d.GetFiles();
+
+
+                int klasor_no = 1;
+                int dosya_sayisi = file.Length;
+                progressBar1.Visible = true;
+                progressBar1.Maximum = dosya_sayisi;
+                progressBar1.Value = 1;
+                progressBar1.Step = 1;
+                MessageBox.Show(dosya_sayisi.ToString());
+                // dosya sayısı kontrol başlangıcı............
+
+                if (dosya_sayisi > bolunme_adedi)
+                {
+                    //++++++++++foreach dosyalar döngü başlangıcı++++++++++++++++++++++++++++++4
+
+                    #region foreach kontrol
+                    foreach (FileInfo dosya in file)
+                    {
+
+                        sayac++;
+                        #region sayackontrol
+                        if (sayac > bolunme_adedi)
+                        {
+                            klasor_no++;
+                            sayac = 1;
+
+
+                        }
+
+                        #endregion
+                        string yeniyol = foldername + "\\" + dizin + "_"+klasor_no;
+
+
+                        #region directoryKontrol
+                        if (Directory.Exists(yeniyol) == true)
+                        {
+
+                            dosya.MoveTo(Path.Combine(yeniyol, dosya.Name));
+                            progressBar1.PerformStep();
+
+                        }
+
+
+                        else
+                        {
+                            Directory.CreateDirectory(yeniyol);
+                            dosya.MoveTo(Path.Combine(yeniyol, dosya.Name));
+                            progressBar1.PerformStep();
+                        }
+                        #endregion
+
+                        Application.DoEvents();
+
+                        //MessageBox.Show(tamyol+":..................."+dosya.FullName);
+                    }
+                    #endregion
+
+                    //++++++++++foreach dosyalar döngü bitimi++++++++++++++++++++++++++++++4
+                }
+
+                else
+                {
+                    return;
+                }
+
+                progressBar1.Visible = false;
+                MessageBox.Show("..............klasör bölme işlemi tamamlandı.........");
+            }
+            
+        }
+
+        private void hakkındaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bir klasör içerisindeki dosyaları belirtilen adede böler ve ana klasör ile aynı isimde olan numaralandırılmış alt klasörlere kopyalar...\n\n Yazan:Caner KOCAMAZ.\n E-posta:kocamaz.caner@gmail.com");
+        }
+                //dosya sayısı kontrol bitiş............
+
+                 
+        
+        }
+    }
